dfa463b6b0eb0ecd5837684cdb365cea3d29a7aa
Faus <cuentadefaustinob@gmail.com>
2024-11-26 19:29:14 -0300

    Add plugin-replace requirement

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import replace from '@rollup/plugin-replace'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replace({
+      'new URL(': 'new globalThis.URL(',
+      delimiters: ['', ''],
+      preventAssignment: true
+    })
+  ],
   base: process.env.NODE_ENV === "production" ? "/static/" : "",
+  define: {
+    "process.env": process.env,
+    VITE_BACKEND_URL: process.env.VITE_BACKEND_URL,
+  },
   server: {
     proxy: {
       "/tasks/api/v1": {
