390ec9424a8f6a099179557c06516fc83e998a95
Faus <cuentadefaustinob@gmail.com>
2024-11-26 16:47:14 -0300

    added dj_database_url requirement

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from corsheaders.defaults import default_headers
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,10 +83,7 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default="sqlite://db.sqlite3") # Try to use DATABASE_URL or else use default
 }
 
 
