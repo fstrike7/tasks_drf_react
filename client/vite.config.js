4e383cd6d66d8dd89397b84af5af50f9349c89ab
Faus <cuentadefaustinob@gmail.com>
2024-11-26 18:27:01 -0300

    vite.config for production environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/static/",
+  base: process.env.NODE_ENV === "production" ? "/static/" : "",
   server: {
     proxy: {
-      '/tasks/api/v1': {
-        target: 'http://localhost:8000',
+      "/tasks/api/v1": {
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
