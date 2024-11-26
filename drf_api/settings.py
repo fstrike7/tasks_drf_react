21cedbaa12ae3ac9e5eb53164f8b113a9fd252b3
Faus <cuentadefaustinob@gmail.com>
2024-11-26 17:01:17 -0300

    Fix settings.py typo

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoiise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
